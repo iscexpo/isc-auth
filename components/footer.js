856469b93923c6f255726d890b96fcb0c2354f5c
Icon Skills & Career Expo <iconcpi2022@gmail.com>
2026-08-21 20:26:24 +0600

    Add files via upload

---
+++ components/footer.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import styles from './footer.module.css'
+import { version } from 'package.json'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <hr />
+      <ul className={styles.navItems}>
+        <li className={styles.navItem}><a href='http://localhost'>Documentation</a></li>
+        <li className={styles.navItem}><a href='https://www.npmjs.com/package/isc-auth'>NPM</a></li>
+        <li className={styles.navItem}><a href='https://github.com/iscexpo/isc-auth-example'>GitHub</a></li>
+        <li className={styles.navItem}><Link href='/policy'><a>Policy</a></Link></li>
+        <li className={styles.navItem}><em>{version}</em></li>
+      </ul>
+    </footer>
+  )
+}

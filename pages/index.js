62d74a3992acf2650d791957bec10759313377a5
Icon Skills & Career Expo <iconcpi2022@gmail.com>
2026-08-21 20:27:14 +0600

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from 'components/layout'
+
+export default function Page() {
+  return (
+    <Layout>
+      <h1>ISCAuth Example</h1>
+      <p>
+        This is an example site to demonstrate how to use <a href='http://localhost'>ISCAuth</a> for authentication.
+      </p>
+    </Layout>
+  )
+}
